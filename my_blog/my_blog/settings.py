5bc94db11083ab5354cbe9cb2aaa500c732b257e
Julian Lobon <julitolos6@gmail.com>
2023-08-14 15:32:07 -0500

    consultas a base de datos

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Author, Entry
 
-# Create your views here.
+# Consulta en base de datos atrabes de objetos .
+def queries(request):
+    #obtener todos los elementos de un objeto
+    authors = Author.objects.all()
+    
+    #obtener datos por filtro 
+    filtrado = Author.objects.filter(email='edward69@example.net')
+    
+    #obtener un unico datos por filtrado
+    author = Author.objects.get(id=24)
+    
+    #Limites de las consultas para solo n elementos
+    limits = Author.objects.all()[:11]
+    
+    #obtener 10 valor saltando los 5 primeros
+    offser = Author.objects.all()[5:10]
+    
+    #obtener todos los metodos ordenados 
+    order = Author.objects.all().order_by('email')
+    
+    #obtener todos los metodos cuyo id sea menor a 17
+    filter = Author.objects.filter(id__lte=17)
+    
+    #obtener todos los valores que contienen la palabra yes 
+    yes = Author.objects.filter(name__contains='yes')
+    
+    
+    return render(request, 'post/queries.html', {'authors': authors, 'filtrado': filtrado, 'author': author, 'limits' : limits, 'offser' : offser, 
+                                                'order' : order, 'filter' : filter, 'yest' : yes })
