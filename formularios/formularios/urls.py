959fdc5cd87c62e6940a8d2bb402222db0f70369
Julian Lobon <julitolos6@gmail.com>
2023-08-18 14:36:22 -0500

    Formulario con html Post

--- formularios/formularios/urls.py
+++ formularios/formularios/urls.py
@@ -20,6 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('form/', views.form,  name="form"),
-    path('goal/', views.goal, name="goal" )
+    path('get/form/', views.getform,  name="form"),
+    path('get/goal/', views.getgoal, name="goal" ),
+    path('post/form/', views.postform,  name="postform"),
+    path('post/goal/', views.postgoal, name="postgoal" ),
 ]
--- formularios/formularios/views.py
+++ formularios/formularios/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def form(request):
+def getform(request):
     return render(request,'form.html',{})
 
-def goal(request):
+def getgoal(request):
     if request.method != 'GET':
-        return HttpResponse('el POST no esta Soportadopor esta ruta')
+        return HttpResponse('el POST no esta Soportado por esta ruta')
 
     name = request.GET['name']
     return render(request,'succes.html', {'name': name})
+
+def postform(request):
+    return render(request, 'postform.html', {})
+
+def postgoal(request):
+    #estrctura de guarda
+        if request.method != 'POST':
+            return HttpResponse('el GET no esta Soportado por esta ruta')
+        
+        info = request.POST['info']
+        return render(request, 'postsucces.html', {'info' : info})
+    
