d544c923382ef4b3a8283e0b7c7ae23681f01e25
Julian Lobon <julitolos6@gmail.com>
2023-08-23 09:04:27 -0500

    Administracion de Grupos y Usuarios, Requistro de panel de admin

--- ecommerce/stock/models.py
+++ ecommerce/stock/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from datetime import date
 
 class Product(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False) 
     short_description = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
     stok = models.IntegerField(default=20)
-    
+    discount_until = models.DateField(default=date.today)
     def __str__(self):
         return self.name
-    
