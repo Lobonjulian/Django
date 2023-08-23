9ca6b2b5832d2039eaffafe8878fdc5faac9d997
Julian Lobon <julitolos6@gmail.com>
2023-08-23 09:26:23 -0500

    uso del model admin

--- ecommerce/stock/admin.py
+++ ecommerce/stock/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description','stok',)
+    search_fields = ('name','short_description')
+    list_filter = ('discount_until','stok',)
+    date_hierarchy = 'discount_until'
+
+admin.site.register(Product, ProductAdmin)
