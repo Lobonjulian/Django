b76c03b784186a1c932b173b8931279d0a76c4a5
Julian Lobon <julitolos6@gmail.com>
2023-08-24 15:22:04 -0500

    practica admin buscador y listas

--- Agenda/contactos/views.py
+++ Agenda/contactos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Contacto
 
-# Create your views here.
+def index(request):
+    # nos da todos los valores que este si no existe nos lo da en blanco
+    contactos = Contacto.objects.filter(name__contains=request.GET.get('search', ''))
+    
+    context = {
+        'contactos': contactos
+    }
+    return render(request, 'contactos/index.html', context)
