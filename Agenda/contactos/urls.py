7a44950727d281ef26f6c6de3f6ae948cb478670
Julian Lobon <julitolos6@gmail.com>
2023-08-25 16:29:00 -0500

    Practicas CRUD Contactos #2

--- Agenda/contactos/urls.py
+++ Agenda/contactos/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='contactos'),
     path('view/<int:id>', views.view, name='contacto_view'),
     path('edit/<int:id>', views.edit, name='contacto_edit'),
+    path('create/', views.create, name='contacto_create'),
 ]
--- Agenda/contactos/views.py
+++ Agenda/contactos/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from unicodedata import name
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Contacto
 from .forms import ContactosForm
+
 def index(request):
     # nos da todos los valores que este si no existe nos lo da en blanco
     contactos = Contacto.objects.filter(name__contains=request.GET.get('search', ''))
@@ -18,10 +21,37 @@ def view(request, id):
     return render(request, 'contactos/details.html', context)
 
 def edit(request, id):
+    contacto = Contacto.objects.get(id=id)
+        
     if(request.method == 'GET'):
-        contacto = Contacto.objects.get(id=id)
         form = ContactosForm(instance = contacto)
+        context = {
+            'form': form,
+            'id': id        
+        }
+        return render(request, 'contactos/edit.html', context)
+    
+    if(request.method == 'POST'):
+        form = ContactosForm(request.POST, instance = contacto)
+        if form.is_valid():
+            form.save()
+        context = {
+            'form': form,
+            'id': id
+        }
+        messages.success(request, 'Contacto actualizado')
+        return render(request, 'contactos/edit.html', context) 
+    
+def create(request):
+    if(request.method == 'GET'):
+        form = ContactosForm()
         context = {
             'form': form
         }
-    return render(request, 'contactos/create.html', context)
+        return render(request, 'contactos/create.html', context)
+    
+    if request.method == 'POST':
+        form =  ContactosForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('contactos')
